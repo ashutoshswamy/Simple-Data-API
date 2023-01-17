71628d3cb986d7bfb37ffcece401ca5b30c08840
Ashutosh Swamy <76511506+devashu397@users.noreply.github.com>
2023-01-17 22:41:13 +0530

    API Version - v1.0.0

---
+++ test/menProfile.js
@@ -0,0 +1,14 @@
+const axios = require("axios");
+
+const url = "https://simple-data-api.devashu397.repl.co/men/profile";
+
+axios
+  .get(url)
+  .then((res) => {
+    console.log(res.data.firstName);
+    console.log(res.data.lastName);
+    console.log(res.data.email);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
